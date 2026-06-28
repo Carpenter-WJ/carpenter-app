8c23e476b92d4aa04454fe268fb1cc3577414a59
squitle88 <squitle88@nate.com>
2026-06-28 11:01:57 +0900

    SW 캐시 v5: index.html 네트워크 우선으로 변경 (Firebase redirect 흐름 수정)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'moksujilji-v4';
-const FILES = ['./index.html', './manifest.json', './icon.svg'];
+const CACHE = 'moksujilji-v5';
+const FILES = ['./manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
@@ -10,8 +10,14 @@ self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys =>
     Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
   ));
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
+  // index.html은 항상 네트워크에서 가져옴 (Firebase redirect 흐름 보장)
+  if (e.request.url.endsWith('/') || e.request.url.includes('index.html')) {
+    e.respondWith(fetch(e.request).catch(() => caches.match('./index.html')));
+    return;
+  }
   e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
